f2966d188e70e5b2746d402e3ea2fd2b502e86f4
mateusaraujos <silva.mateusaraujo98@gmail.com>
2025-06-30 19:40:01 -0300

    Adiciona a função de trocar texto

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ console.log(`Iniciando o ${projeto}`);
 function mostrarMensagem() {
     alert("Bem-vindo ao projeto!");
 }
+
+function trocarTexto() {
+    const mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.innerText = "Texto modificado com JS!";
+    mensagemDiv.style.color = "purple";
+    mensagemDiv.style.fontSize = "24px";
+}
