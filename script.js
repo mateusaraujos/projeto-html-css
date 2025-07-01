162a92efe1812e33cfbf595ae4c5fe84c3ce08bb
mateusaraujos <silva.mateusaraujo98@gmail.com>
2025-07-01 19:26:23 -0300

    Adiciona um pequeno formulário

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const projeto = "projeto-html-css";
-
 console.log(`Iniciando o ${projeto}`);
+
+const form = document.getElementById("form-contato");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    validarFormulario();
+})
 
 function mostrarMensagem() {
     alert("Bem-vindo ao projeto!");
@@ -11,4 +17,18 @@ function trocarTexto() {
     mensagemDiv.innerText = "Texto modificado com JS!";
     mensagemDiv.style.color = "purple";
     mensagemDiv.style.fontSize = "24px";
+}
+
+function validarFormulario() {
+    const nome = document.getElementById("nome").value.trim(); // Remove espaços em branco
+	const email = document.getElementById("email").value.trim();
+
+    if (nome === "" || email === "") {
+        alert("Por favor, preencha todos os campos corretamente.");
+        return;
+    }
+
+    console.log(`Nome: ${nome}`);
+    console.log(`E-mail: ${email}`);
+    alert("Formulário enviado com sucesso!");
 }
